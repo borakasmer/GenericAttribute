2acd259538b704e1b7301a05175da637602d6513
Bora KAŞMER <bora@borakasmer.com>
2023-03-17 19:17:50 +0300

    Add files via upload
    Create GenericAttribute Console Application

---
+++ GenericAttribute/Program.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entities;
+using Repository;
+
+//Console.WriteLine("Hello, World!");
+Users newUser = new()
+{
+    Name = "Baris",
+    LastName = "Manco",
+    UserName = "bmanco",
+    Email = "baris@barismanco.com",
+    PasswordHash = "12345",
+    Gsm = "3334",
+    IsAdmin = true
+};
+using (GeneralRepository<Users> repository = new())
+{
+    repository.Insert(newUser, true);
+}
